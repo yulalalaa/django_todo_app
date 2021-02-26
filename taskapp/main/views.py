fb7086db44206b31f072ac76b65b2e14c25cc15c
Yulia <yagoyulia@gmail.com>
2021-02-26 22:13:01 +0100

    without deleting and editing options yet

--- taskapp/main/views.py
+++ taskapp/main/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 
 
 def index (request):
-	tasks = Task.objects.all()
+	tasks = Task.objects.order_by('-id')
 	return render (request, 'main/index.html', {'title': "Main Page", 'tasks': tasks})
 
 def about (request):
 	return render (request, 'main/about.html')
+
+
+def create (request):
+	error = ''
+	if request.method == 'POST':
+		form = TaskForm (request.POST)
+		if form.is_valid ():
+			form.save ()
+			#return redirect ('home')
+
+		else:
+			error = 'Not correct'
+
+	form = TaskForm ()
+	context = {
+	    'form': form
+	    #'error':error
+	}
+	return render (request, 'main/create.html', context)
