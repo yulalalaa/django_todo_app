bd94c334080dc9aa79148cdc34eb4f2191bc284f
Yulia <yagoyulia@gmail.com>
2021-02-26 14:55:52 +0100

    second commit

--- taskapp/main/models.py
+++ taskapp/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Task (models.Model):
+	title = models.CharField ('Name', max_length=50)
+	task = models.TextField ('Description')
+
+	def __str__ (self):
+		return self.title
+
+	class Meta:
+		verbose_name='Task'
+		verbose_name_plural='Tasks'
+
--- taskapp/main/views.py
+++ taskapp/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
 
 
 def index (request):
-	return render (request, 'main/index.html')
+	tasks = Task.objects.all()
+	return render (request, 'main/index.html', {'title': "Main Page", 'tasks': tasks})
 
 def about (request):
 	return render (request, 'main/about.html')
