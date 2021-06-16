2cf2138d944a21955b62e4c5cf068d117bda77d1
Yulia <yagoyulia@gmail.com>
2021-06-16 22:23:43 +0200

    prep for dep

--- taskapp/taskapp/settings.py
+++ taskapp/taskapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -118,4 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+# Activate Django Heroku
+django_heroku.settings(locals())
